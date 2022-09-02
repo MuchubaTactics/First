e5dcadc6c28a78a6e3f8e3cf186b5dbe778151bd
hp <hp@MUCHUBATACTICS>
2022-09-02 22:13:24 +0300

    Add project files.

---
+++ small/main.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <ctime>
+#include <cstdlib>
+#include "func.h"
+#include "strTing.h"
+
+//function declarations
+double Fmax(double[], int);
+double Fmin(double[], int);
+double Faverage(double[], int);
+void Fsort(double*, int);
+void swap(double*, double*);
+void ArrTing(void);
+
+int main(void)
+{
+	ArrTing();
+	return 0;
+}
