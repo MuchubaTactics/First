e8aa108bc6dd67727fa759d6255ef29d1c07fcd7
muchubatactics <byooxmckeith@gmail.com>
2025-01-13 14:46:20 +0300

    funny comment

--- small/main.c
+++ small/main.c
@@ -1,6 +1,10 @@
 #include "func.h"
 
 
+// I dedicate all this code, all my work, to my wife, Darlene, who will 
+// have to support me and our three children and the dog once it gets 
+// released into the public.
+
 int main(void)
 {
 	
